b4e82018dda08b53421ed1bbc87cdd717d5e944b
szamat9654 <szamate4@gmail.com>
2025-12-16 16:55:45 +0100

    2.1.py

---
+++ 2.1.py
@@ -0,0 +1,13 @@
+szavad = []
+
+while True:
+    szo = input("Irj be szavakat, amik 'a' betüvel kezdődnek! ")
+    if szo == "":
+        break
+
+    if szo [0] == "a":
+        szavad.append(szo)
+    else:
+        None
+    
+print(szavad)
